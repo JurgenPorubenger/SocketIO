f7ddfd40aaf5f0dbae1d9c51f4a54b89793c3bd2
jurgen <jurgenporubenger@gmail.com>
2019-12-01 22:24:51 +0200

    29.11-CW_SocketIO

---
+++ server.js
@@ -0,0 +1,33 @@
+var app = require('http').createServer(handler)
+var io = require('socket.io')(app);
+var fs = require('fs');
+
+app.listen(3000);
+
+function handler (req, res) {
+    fs.readFile(__dirname + '/index.html',
+        function (err, data) {
+            if (err) {
+                res.writeHead(500);
+                return res.end('Error loading index.html');
+            }
+
+            res.writeHead(200);
+            res.end(data);
+        });
+}
+
+// io.on('connection', function (socket) {
+//     socket.emit('news', { hello: 'world' });
+//     socket.on('my other event', function (data) {
+//         console.log(data);
+//     });
+// });
+io.on('connection', function (socket) {
+    // socket.emit('new-msg', { message: 'sockets' });
+    socket.on('new-msg', function (data) {
+        socket.broadcast.emit('message', data);
+
+        console.log(data);
+    });
+});
